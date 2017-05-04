ec4ecfdc91347c7caf2a4fc3c289927c7c7237ae
D065057 <thedomdom96@gmail.com>
2017-05-04 22:48:59 +0200

    Moved "getCharge" method from Movie to Price enum

--- src/Movie.java
+++ src/Movie.java
@@ -8,23 +8,7 @@ public class Movie {
     }
 
     double getCharge(int daysRented) {
-        double charge = 0;
-        switch (price) {
-            case REGULAR:
-                charge += 2;
-                if (daysRented > 2)
-                    charge += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                charge += daysRented * 3;
-                break;
-            case CHILDRENS:
-                charge += 1.5;
-                if (daysRented > 3)
-                    charge += (daysRented - 3) * 1.5;
-                break;
-        }
-        return charge;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
--- src/Price.java
+++ src/Price.java
@@ -3,4 +3,23 @@ public enum Price {
     NEW_RELEASE,
     CHILDRENS;
 
+    double getCharge(int daysRented) {
+        double charge = 0;
+        switch (this) {
+            case REGULAR:
+                charge += 2;
+                if (daysRented > 2)
+                    charge += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                charge += daysRented * 3;
+                break;
+            case CHILDRENS:
+                charge += 1.5;
+                if (daysRented > 3)
+                    charge += (daysRented - 3) * 1.5;
+                break;
+        }
+        return charge;
+    }
 }
