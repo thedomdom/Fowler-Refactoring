b532395cb841b6d1ca9301b003b3bdc02d470d6c
D065057 <thedomdom96@gmail.com>
2017-05-04 21:41:55 +0200

    Extracted and moved FrequentRenterPoints calculation

--- src/Rental.java
+++ src/Rental.java
@@ -7,7 +7,7 @@ class Rental {
         daysRented = newdaysRented;
     }
 
-    double getCharge() {
+    public double getCharge() {
         double charge = 0;
         switch (movie.getPriceCode()) {
             case Movie.REGULAR:
@@ -25,6 +25,12 @@ class Rental {
                 break;
         }
         return charge;
+    }
+
+    public int getFrequentRenterPoints() {
+        // add bonus for a two day new release rental
+        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+        return 1;
     }
 
     public int getDaysRented() {
