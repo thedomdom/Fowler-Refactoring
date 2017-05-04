46eedba50de5861235404850b3b805f49ec62999
D065057 <thedomdom96@gmail.com>
2017-05-04 21:28:01 +0200

    Moved "amountFor" method from Customer to Rental class and renamed it

--- src/Rental.java
+++ src/Rental.java
@@ -7,6 +7,26 @@ class Rental {
         daysRented = newdaysRented;
     }
 
+    double getCharge() {
+        double charge = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                charge += 2;
+                if (daysRented > 2)
+                    charge += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                charge += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                charge += 1.5;
+                if (daysRented > 3)
+                    charge += (daysRented - 3) * 1.5;
+                break;
+        }
+        return charge;
+    }
+
     public int getDaysRented() {
         return daysRented;
     }
