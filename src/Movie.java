23075ddd12b8860af62f8ff99bd6d4f4834e527d
D065057 <thedomdom96@gmail.com>
2017-05-04 23:07:11 +0200

    Moved "getCharge" method onto Price class

--- src/Movie.java
+++ src/Movie.java
@@ -3,45 +3,41 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price price;
 
     public Movie(String title, int priceCode) {
         this.title = title;
-        this.priceCode = priceCode;
+        this.setPriceCode(priceCode);
     }
 
     double getCharge(int daysRented) {
-        double charge = 0;
-        switch (priceCode) {
-            case Movie.REGULAR:
-                charge += 2;
-                if (daysRented > 2)
-                    charge += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                charge += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                charge += 1.5;
-                if (daysRented > 3)
-                    charge += (daysRented - 3) * 1.5;
-                break;
-        }
-        return charge;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
         // add bonus for a two day new release rental
-        if ((priceCode == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
         return 1;
     }
 
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
 
-    public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
