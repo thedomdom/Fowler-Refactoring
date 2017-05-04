2dc792bde6eeb53c33a9143db3f3dedabce533c1
D065057 <thedomdom96@gmail.com>
2017-05-04 22:19:52 +0200

    Moved "getCharge" method onto Movie class

--- src/Rental.java
+++ src/Rental.java
@@ -8,23 +8,7 @@ class Rental {
     }
 
     public double getCharge() {
-        double charge = 0;
-        switch (movie.getPriceCode()) {
-            case Movie.REGULAR:
-                charge += 2;
-                if (daysRented > 2)
-                    charge += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                charge += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                charge += 1.5;
-                if (daysRented > 3)
-                    charge += (daysRented - 3) * 1.5;
-                break;
-        }
-        return charge;
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints() {
