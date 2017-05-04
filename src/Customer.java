80ed66aa5707d299278ed829a52c006340971ab1
D065057 <thedomdom96@gmail.com>
2017-05-04 21:55:47 +0200

    Replaced Vector with a list for less code

--- src/Customer.java
+++ src/Customer.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 class Customer {
     private String name;
-    private Vector rentals = new Vector();
+    private List<Rental> rentals = new LinkedList<>();
 
     public Customer(String newname) {
         name = newname;
     }
 
-    public void addRental(Rental arg) {
-        rentals.addElement(arg);
+    public void addRental(Rental rental) {
+        rentals.add(rental);
     }
 
     public String getName() {
@@ -20,17 +20,16 @@ class Customer {
     public String statement() {
         double totalCharge = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
+        for (Rental rental : rentals) {
+            frequentRenterPoints += rental.getFrequentRenterPoints();
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalCharge += each.getCharge();
+            result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(rental.getCharge()) + "\n";
+            totalCharge += rental.getCharge();
         }
+
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalCharge) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
